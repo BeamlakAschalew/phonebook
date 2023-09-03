25fdcb7b36f1f236901a770ebbf02154ba4448fe
Beamlak Aschalew <birrletej12@gmail.com>
2023-09-03 09:28:11 +0300

    Update??

--- main.cpp
+++ main.cpp
@@ -1,208 +1,30 @@
 ///// LET'S DO THIS!
-//#include <iostream>
-//#include <fstream>
-//
-//using namespace std;
-//
-//ifstream input;
-//ofstream output;
-//
-//struct Contact {
-//    string firstName, lastName;
-//    char gender;
-//    int phoneNumber;
-//};
-//
-//void openInputFile(string fileLocation) {
-//    input.open(fileLocation, ios::binary);
-//}
-//
-//void openOutputFile(string fileLocation) {
-//    output.open(fileLocation, ios::binary | ios::app);
-//}
-//
-//void displayContact() {
-//    openInputFile("../contacts.bin");
-//    if (!input.is_open()) {
-//        cout << "Problem opening file.";
-//        exit(1);
-//    } else {
-//        while (!input.eof()) {
-//            char c;
-//            input.read((char*) &c, sizeof(c));
-//            cout << c;
-//        }
-//    }
-//}
-//
-//void searchContact() {
-//
-//}
-//
-//void addContact() {
-//    Contact c;
-//    system("cls");
-//    cout << "Enter first name: ";
-//    getline(cin.ignore(), c.firstName.append(" "));
-//    cout << "Enter last name: ";
-//    getline(cin, c.lastName);
-//    cout << "Enter gender: ";
-//    cin >> c.gender;
-//    cout << "Enter phone number";
-//    cin >> c.phoneNumber;
-//
-//    openOutputFile("../contacts.bin");
-//    if (!output.is_open()) {
-//        cout << "Problem opening file.";
-//        exit(1);
-//    } else {
-//        output.write((char*) &c.firstName, c.firstName.size());
-//        output.close();
-//    }
-//
-//}
-//void modifyContact() {
-//
-//}
-//
-//void deleteContact() {
-//
-//}
-//
-//
-//int main() {
-//
-//    bool navigating = true;
-//    int menuChoice;
-//
-//    while (navigating) {
-//        cout << "1) View all contacts\n2) Search for a contact\n3) Add a new contact\n4) Modify existing contact\n5) Delete existing contact";
-//        cin >> menuChoice;
-//
-//        switch (menuChoice) {
-//            case 1: {
-//                displayContact();
-//            }
-//                break;
-//            case 2: {
-//                searchContact();
-//            }
-//                break;
-//            case 3: {
-//                addContact();
-//            }
-//                break;
-//            case 4: {
-//                modifyContact();
-//            }
-//                break;
-//            case 5: {
-//                deleteContact();
-//            }
-//                break;
-//            default: cout << "Invalid option.";
-//        }
-//    }
-//
-//    return 0;
-//}
-
-//#include <iostream>
-//#include <fstream>
-//#include <string>
-//
-//// Structure to hold contact information
-//struct Contact {
-//    std::string firstName;
-//    std::string lastName;
-//    char gender;
-//    std::string phoneNumber;
-//};
-//
-//// Function to write contact data to a binary file
-//void writeContactsToFile(const std::string& filename, const Contact* contacts, int numContacts) {
-//    std::ofstream outFile(filename, std::ios::binary);
-//    if (!outFile) {
-//        std::cerr << "Error opening file for writing." << std::endl;
-//        return;
-//    }
-//
-//    for (int i = 0; i < numContacts; ++i) {
-//        outFile.write(contacts[i].firstName.c_str(), contacts[i].firstName.length() + 1);
-//        outFile.write(contacts[i].lastName.c_str(), contacts[i].lastName.length() + 1);
-//        outFile.write(&contacts[i].gender, sizeof(char));
-//        outFile.write(contacts[i].phoneNumber.c_str(), contacts[i].phoneNumber.length() + 1);
-//    }
-//
-//    outFile.close();
-//}
-//
-//// Function to read contact data from a binary file
-//void readContactsFromFile(const std::string& filename, Contact* contacts, int numContacts) {
-//    std::ifstream inFile(filename, std::ios::binary);
-//    if (!inFile) {
-//        std::cerr << "Error opening file for reading." << std::endl;
-//        return;
-//    }
-//
-//    for (int i = 0; i < numContacts; ++i) {
-//        std::getline(inFile, contacts[i].firstName, '\0');
-//        std::getline(inFile, contacts[i].lastName, '\0');
-//        inFile.read(&contacts[i].gender, sizeof(char));
-//        std::getline(inFile, contacts[i].phoneNumber, '\0');
-//    }
-//
-//    inFile.close();
-//}
-//
-//int main() {
-//    // Example data
-//    Contact contacts[] = {
-//            {"John", "Doe", 'M', "123-456-7890"},
-//            {"Jane", "Smith", 'F', "987-654-3210"}
-//    };
-//    int numContacts = sizeof(contacts) / sizeof(contacts[0]);
-//
-//    // Write data to binary file
-//    writeContactsToFile("../contacts.bin", contacts, numContacts);
-//
-//    // Read data from binary file
-//    Contact readContacts[numContacts];
-//    readContactsFromFile("../contacts.bin", readContacts, numContacts);
-//
-//    // Display read data
-//    for (int i = 0; i < numContacts; ++i) {
-//        std::cout << "First Name: " << readContacts[i].firstName << std::endl;
-//        std::cout << "Last Name: " << readContacts[i].lastName << std::endl;
-//        std::cout << "Gender: " << readContacts[i].gender << std::endl;
-//        std::cout << "Phone Number: " << readContacts[i].phoneNumber << std::endl;
-//        std::cout << "------------------------" << std::endl;
-//    }
-//
-//    return 0;
-//}
-
 #include <iostream>
 #include <fstream>
-#include <string>
 
-// Structure to hold contact information
+using namespace std;
+
+ifstream input;
+ofstream output;
+
 struct Contact {
-    std::string firstName;
-    std::string lastName;
+    string firstName;
+    string lastName;
     char gender;
-    std::string phoneNumber;
+    string phoneNumber;
+    int id;
 };
 
 // Function to write a contact to a binary file
-void writeContactToFile(const std::string& filename, const Contact& contact) {
-    std::ofstream outFile(filename, std::ios::binary | std::ios::app);
+void addContact(const string& filename, const Contact& contact) {
+    ofstream outFile(filename, ios::binary | ios::app);
     if (!outFile) {
-        std::cerr << "Error opening file for writing." << std::endl;
+        cerr << "Error opening file for writing." << endl;
         return;
     }
 
-    outFile.write(contact.firstName.c_str(), contact.firstName.length() + 1);
+    outFile.write(&contact.firstName[0], contact.firstName.length() + 1);
+    cout << "String size: " << contact.firstName.length() + 1 << endl;
     outFile.write(contact.lastName.c_str(), contact.lastName.length() + 1);
     outFile.write(&contact.gender, sizeof(char));
     outFile.write(contact.phoneNumber.c_str(), contact.phoneNumber.length() + 1);
@@ -211,10 +33,10 @@ void writeContactToFile(const std::string& filename, const Contact& contact) {
 }
 
 // Function to read contact data from a binary file
-void readContactsFromFile(const std::string& filename) {
-    std::ifstream inFile(filename, std::ios::binary);
+void displayContacts(const string& filename) {
+    ifstream inFile(filename, ios::binary);
     if (!inFile) {
-        std::cerr << "Error opening file for reading." << std::endl;
+        cerr << "Error opening file for reading." << endl;
         return;
     }
 
@@ -237,26 +59,170 @@ void readContactsFromFile(const std::string& filename) {
         }
 
         if (!contact.firstName.empty() || !contact.lastName.empty()) {
-            std::cout << "First Name: " << contact.firstName << std::endl;
-            std::cout << "Last Name: " << contact.lastName << std::endl;
-            std::cout << "Gender: " << contact.gender << std::endl;
-            std::cout << "Phone Number: " << contact.phoneNumber << std::endl;
-            std::cout << "------------------------" << std::endl;
+            cout << "ID: " << contact.id << endl;
+            cout << "First Name: " << contact.firstName << endl;
+            cout << "Last Name: " << contact.lastName << endl;
+            cout << "Gender: " << contact.gender << endl;
+            cout << "Phone Number: " << contact.phoneNumber << endl;
+            cout << "------------------------" << endl;
         }
+
     }
 
     inFile.close();
 }
 
+void searchContact(string filename) {
+    string fname, lname;
+    cout << "Enter first name: ";
+    getline(cin.ignore(), fname);
+    cout << "Enter last name: ";
+    getline(cin.ignore(), lname);
+
+    ifstream inFile(filename, ios::binary);
+    if (!inFile) {
+        cerr << "Error opening file for reading." << endl;
+        return;
+    }
+    int id = 1;
+    while (!inFile.eof()) {
+        Contact contact;
+
+        char ch;
+
+        contact.id = id;
+        while (inFile.get(ch) && ch != '\0') {
+            contact.firstName += ch;
+        }
+
+        while (inFile.get(ch) && ch != '\0') {
+            contact.lastName += ch;
+        }
+
+        inFile.read(&contact.gender, sizeof(char));
+
+        while (inFile.get(ch) && ch != '\0') {
+            contact.phoneNumber += ch;
+        }
+
+        if (!contact.firstName.empty() || !contact.lastName.empty()) {
+            if (contact.firstName == fname) {
+                cout << "ID: " << contact.id << endl;
+                cout << "First Name: " << contact.firstName << endl;
+                cout << "Last Name: " << contact.lastName << endl;
+                cout << "Gender: " << contact.gender << endl;
+                cout << "Phone Number: " << contact.phoneNumber << endl;
+                cout << "------------------------" << endl;
+            }
+        }
+        id++;
+    }
+
+    inFile.close();
+}
+
+void modifyContact(string filename) {
+    int idInput;
+    cout << "Enter first name: ";
+    cin >> idInput;
+
+    ifstream inFile(filename, ios::binary);
+    if (!inFile) {
+        cerr << "Error opening file for reading." << endl;
+        return;
+    }
+    int id = 1;
+    int charCount = 0;
+    while (id <= idInput && !inFile.eof()) {
+        Contact contact;
+
+
+        char ch;
+
+        contact.id = id;
+        while (inFile.get(ch) && ch != '\0') {
+            contact.firstName += ch;
+            charCount++;
+        }
+
+        while (inFile.get(ch) && ch != '\0') {
+            contact.lastName += ch;
+            charCount++;
+        }
+
+        inFile.read(&contact.gender, sizeof(char));
+        charCount++;
+
+        while (inFile.get(ch) && ch != '\0') {
+            contact.phoneNumber += ch;
+            charCount++;
+
+        }
+
+        if (!contact.firstName.empty() || !contact.lastName.empty()) {
+            if (contact.id == idInput) {
+                streampos pos = inFile.tellg();
+
+                // Move the file pointer back to the start of the contact's record
+                if (idInput == 1) {
+                    inFile.seekg(0, ios::beg);
+                } else {
+                    inFile.seekg(charCount);
+                }
+
+               cout << inFile.tellg();
+
+                // Write the modified contact back to the file
+               // inFile.write(reinterpret_cast<const char*>(&contact), sizeof(Contact));
+            }
+        }
+        id++;
+        cout << "itaeration finished" << endl;
+        break;
+    }
+
+    inFile.close();
+}
+
+
+
+
 int main() {
-    // Example data
-    Contact newContact = {"Alice", "Johnson", 'F', "555-123-4567"};
 
-    // Write new contact data to binary file
-    writeContactToFile("../contacts.bin", newContact);
+    bool navigating = true;
+    int menuChoice;
 
-    // Read all contact data from binary file
-    readContactsFromFile("../contacts.bin");
+    Contact newContact = {"ABC", "uiop", 'M', "555-123-4567"};
+
+    while (navigating) {
+        cout << "1) View all contacts\n2) Search for a contact\n3) Add a new contact\n4) Modify existing contact\n5) Delete existing contact";
+        cin >> menuChoice;
+
+        switch (menuChoice) {
+            case 1: {
+                displayContacts("../contacts.bin");
+            }
+                break;
+            case 2: {
+               searchContact("../contacts.bin");
+               break;
+            }
+                break;
+            case 3: {
+                addContact("../contacts.bin", newContact);
+            }
+                break;
+            case 4: {
+               modifyContact("../contacts.bin");
+            }
+                break;
+            case 5: {
+               // deleteContact();
+            }
+                break;
+            default: cout << "Invalid option.";
+        }
+    }
 
     return 0;
 }
